2e574607c3454ad02cade8c2b5e7c75ab47b0890
Spike <55534649+Spike-dev@users.noreply.github.com>
2020-10-19 17:59:22 -0600

    Add files via upload

---
+++ inviteTrackerEvent.js
@@ -0,0 +1,14 @@
+module.exports = class Persona {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+
+  saludar() {
+     return "Hola mi nombre es " + this.nombre;
+  }
+
+  get name(){
+    return this.nombre ;
+  }
+
+}
